a27c1893ace23667ead38c0c548be0490672d609
Umud-Isken <uiskenderov3@gmail.com>
2025-02-01 19:15:32 +0400

    commit

--- JavaSE/src/Lessin24_2/Student.java
+++ JavaSE/src/Lessin24_2/Student.java
@@ -4,6 +4,13 @@ public class Student extends Person {
 	public Student(String name) {
 		super(name);
 	}
+	
+
+	@Override
+	public String toString() {
+		return "Student []";
+	}
+
 
 	@Override
 	public void introduce() {
