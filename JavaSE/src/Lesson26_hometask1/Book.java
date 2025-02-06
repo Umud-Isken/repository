cf959ccc0522afdf14027469a98c5e24d860048b
Umud-Isken <uiskenderov3@gmail.com>
2025-02-06 20:33:22 +0400

    t

---
+++ JavaSE/src/Lesson26_hometask1/Book.java
@@ -0,0 +1,7 @@
+package Lesson26_hometask1;
+
+public class Book {
+	String title;
+	String author;
+	int year;
+}
---
+++ JavaSE/src/Lesson26_hometask1/Main.java
@@ -0,0 +1,8 @@
+package Lesson26_hometask1;
+
+public class Main {
+	public static void main(String[] args) {
+		Book book =new Book();
+		System.out.println(book.toString());
+		}
+}
